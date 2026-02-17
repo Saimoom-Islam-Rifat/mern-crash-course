14ee56f07501a0a5e0f6a95f5b5c8c0081d2bf26
Saimoom-Islam-Rifat <saimoom1362@gmail.com>
2026-02-17 13:24:23 +0600

    Final push before deploying

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -9,10 +10,20 @@ await connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
+
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(`Server connection succeded at port ${PORT}`);
+  console.log(`Server connected at port ${PORT}`);
 });
